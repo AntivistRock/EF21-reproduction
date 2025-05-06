590986a20cd32bccbb1070170a9271c554f8a979
Ivan Konyushenko <idkonyushenko@gmail.com>
2025-05-06 12:04:24 +0300

    Fix: fixed ef21plus.py

--- src/optimizer/ef21plus.py
+++ src/optimizer/ef21plus.py
@@ -39,22 +39,20 @@ class EF21Plus(torch.optim.Optimizer):
                     p.grad.data += group["lambda"] * (2 * p.data) / (1 + p.data ** 2) ** 2  # add regularization term from equation (19) of the paper
 
                     b = self._top_k_compression(p.grad.data, group["k"])
-                    m = self._top_k_compression(p.grad.data - self.state[p]['g'], group["k"])  # line 5
+                    m = self._top_k_compression(p.grad.data - self.state[p]['g'], group["k"])
                     
                     B = (p.grad.data - b).flatten().square().sum()
                     M = (p.grad.data - m).flatten().square().sum()
                     
-                    c = M
+                    c = m
                     if B > M:
-                        c = B
+                        c = b
 
                     self.state[p]['g'] += c  # line 6
                 else:
                     c = torch.zeros_like(p)
                 dist.reduce(c, dst=0)  # send c to the master
-
                 if dist.get_rank() == 0:
-                    print('HERE!')
                     self.state[p]['g'] += c / (dist.get_world_size() - 1)
                     p.data -= group['lr'] * self.state[p]['g']
                 dist.broadcast(p, src=0)  # broadcast new parameter to all nodes
